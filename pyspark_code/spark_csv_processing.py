1921a0a7b9bbe5d9b41604d0688cbdf2dc5baf77
theemadnes <alex.mattson@gmail.com>
2016-02-17 22:33:29 -0800

    added some comments

--- pyspark_code/spark_csv_processing.py
+++ pyspark_code/spark_csv_processing.py
@@ -8,7 +8,7 @@ import datetime
 # define params
 s3_target_bucket_name = 'mattsona-public'
 
-sc = SparkContext()
+sc = SparkContext() # create a spark context
 
 # load data file from s3
 print('Loading file from S3...')
@@ -18,10 +18,10 @@ lines = source_file.map(lambda x: x.split(','))
 
 # create filter RDDs to show 'Male' & 'Female' sets, sort them by last name, and save to S3
 time_stamp = datetime.datetime.isoformat(datetime.datetime.now()).replace(':','_') # create a datestamp for both RDDs to be saved
-female_rdd = lines.filter(lambda x: x[4] == 'Female')
-sorted_female_rdd = female_rdd.sortBy(lambda x: x[2], True)
-male_rdd = lines.filter(lambda x: x[4] == 'Male')
-sorted_male_rdd = male_rdd.sortBy(lambda x: x[2], True)
+female_rdd = lines.filter(lambda x: x[4] == 'Female') # [4] is gender
+sorted_female_rdd = female_rdd.sortBy(lambda x: x[2], True) # [2] is last name
+male_rdd = lines.filter(lambda x: x[4] == 'Male') # [4] is gender
+sorted_male_rdd = male_rdd.sortBy(lambda x: x[2], True) # [2] is last name
 
 sorted_female_rdd_to_s3 = sorted_female_rdd.saveAsTextFile('s3://' + s3_target_bucket_name + '/spark_csv_processing/' + time_stamp + '/female')
 sorted_male_rdd_to_s3 = sorted_male_rdd.saveAsTextFile('s3://' + s3_target_bucket_name + '/spark_csv_processing/' + time_stamp + '/male')
