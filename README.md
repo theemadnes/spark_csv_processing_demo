483b1a273a91ca1fb55b0e71bfd3e6a8cd5f35b2
theemadnes <alex.mattson@gmail.com>
2016-02-17 22:30:30 -0800

    added sort by last name

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # spark_csv_processing_demo
 
 Taking a simple data set from https://www.mockaroo.com/ to perform some basic Spark processing transforms to / from S3.
+It will take a data set that has female and male entries, filter them in to separate RDDs, and then sort the results.
+Once that's done, it'll store the sorted results to S3. 
 
 Notes:
 - This uses PySpark, and was testing using Spark 1.5.2 on EMR.
--- pyspark_code/spark_csv_processing.py
+++ pyspark_code/spark_csv_processing.py
@@ -16,11 +16,12 @@ source_file = sc.textFile('s3://mattsona-public/spark_demo_data/mock-data-01.csv
 # split each line on ',' as this is a csv
 lines = source_file.map(lambda x: x.split(','))
 
-# create filter RDDs to show 'Male' & 'Female' sets and save to S3
+# create filter RDDs to show 'Male' & 'Female' sets, sort them by last name, and save to S3
 time_stamp = datetime.datetime.isoformat(datetime.datetime.now()).replace(':','_') # create a datestamp for both RDDs to be saved
 female_rdd = lines.filter(lambda x: x[4] == 'Female')
-# female_rdd = source_file.map(lambda x: x.split(',')).filter(lambda x: lines[4] == 'Female')
+sorted_female_rdd = female_rdd.sortBy(lambda x: x[2], True)
 male_rdd = lines.filter(lambda x: x[4] == 'Male')
+sorted_male_rdd = male_rdd.sortBy(lambda x: x[2], True)
 
-female_rdd_to_s3 = female_rdd.saveAsTextFile('s3://' + s3_target_bucket_name + '/spark_csv_processing/' + time_stamp + '/female')
-male_rdd_to_s3 = male_rdd.saveAsTextFile('s3://' + s3_target_bucket_name + '/spark_csv_processing/' + time_stamp + '/male')
+sorted_female_rdd_to_s3 = sorted_female_rdd.saveAsTextFile('s3://' + s3_target_bucket_name + '/spark_csv_processing/' + time_stamp + '/female')
+sorted_male_rdd_to_s3 = sorted_male_rdd.saveAsTextFile('s3://' + s3_target_bucket_name + '/spark_csv_processing/' + time_stamp + '/male')
